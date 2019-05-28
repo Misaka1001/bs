0bcb42c499c2e1aeea0859137c4b9b577e923a44
Misaka1001 <eugen0822@outlook.com>
2019-05-28 16:24:30 +0800

    修改折线图样式

--- client/index.js
+++ client/index.js
@@ -74,56 +74,127 @@ class Chart {
                     var time = new Date(item.time);
                     return time.getHours() + '时' + time.getMinutes() + '分' + time.getSeconds() + 's'
                 })
-                this.chart.setOption({
-                    xAxis: {
-                        type: "category",
-                        boundaryGap: false,
-                        data: this.time,
-                        axisLabel: { //坐标轴刻度标签的相关设置。
-                            interval: 5,
-                            rotate: "45",
-                        },
-                    },
-                    yAxis: {
-                        boundaryGap: [0, '100%'],
-                        type: 'value',
-                        axisLabel: {
-                            formatter: `{value} (${this.format})`
+
+                let option = {
+                    tooltip: {
+                        trigger: 'axis',
+                        position: function (pt) {
+                            return [pt[0], '10%'];
                         }
                     },
-                    tooltip: {
-                        show: true,
-                        trigger: 'axis'
+                    title: {
+                        left: 'center',
+                        text: '测量数据',
+                    },
+                    toolbox: {
+                        feature: {
+                            dataZoom: {
+                                yAxisIndex: 'none'
+                            },
+                            restore: {},
+                            saveAsImage: {}
+                        }
+                    },
+                    xAxis: {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: this.time
+                    },
+                    yAxis: {
+                        type: 'value',
+                        boundaryGap: [0, '100%']
                     },
                     dataZoom: [{
-                        type: 'slider',
-                        start: 0,
-                        end: 100
-                    },
-                    {
                         type: 'inside',
                         start: 0,
-                        end: 100
-                    }
-                    ],
-                    series: [{
-                        name: this.id,
-                        type: 'line',
-                        smooth: true, //数据光滑过度
-                        symbol: 'none', //下一个数据点
-                        stack: 'a',
-                        areaStyle: {
-                            normal: {
-                                color: 'red'
-                            }
-                        },
-                        data: this.data
+                        end: 10
+                    }, {
+                        start: 0,
+                        end: 10,
+                        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+                        handleSize: '80%',
+                        handleStyle: {
+                            color: '#fff',
+                            shadowBlur: 3,
+                            shadowColor: 'rgba(0, 0, 0, 0.6)',
+                            shadowOffsetX: 2,
+                            shadowOffsetY: 2
+                        }
                     }],
-                    grid: {
-                        bottom: '30%',
-                        left: '20%'
-                    }
-                })
+                    series: [
+                        {
+                            name:this.id,
+                            type:'line',
+                            smooth:true,
+                            symbol: 'none',
+                            sampling: 'average',
+                            itemStyle: {
+                                color: 'rgb(255, 70, 131)'
+                            },
+                            areaStyle: {
+                                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                    offset: 0,
+                                    color: 'rgb(255, 158, 68)'
+                                }, {
+                                    offset: 1,
+                                    color: 'rgb(255, 70, 131)'
+                                }])
+                            },
+                            data: this.data
+                        }
+                    ]
+                };
+                this.chart.setOption(option)
+                // this.chart.setOption({
+                //     xAxis: {
+                //         type: "category",
+                //         boundaryGap: false,
+                //         data: this.time,
+                //         axisLabel: { //坐标轴刻度标签的相关设置。
+                //             interval: 5,
+                //             rotate: "45",
+                //         },
+                //     },
+                //     yAxis: {
+                //         boundaryGap: [0, '100%'],
+                //         type: 'value',
+                //         axisLabel: {
+                //             formatter: `{value} (${this.format})`
+                //         }
+                //     },
+                //     tooltip: {
+                //         show: true,
+                //         trigger: 'axis'
+                //     },
+                //     dataZoom: [{
+                //         type: 'slider',
+                //         start: 0,
+                //         end: 100
+                //     },
+                //     {
+                //         type: 'inside',
+                //         start: 0,
+                //         end: 100
+                //     }
+                //     ],
+                //     series: [{
+                //         name: this.id,
+                //         type: 'line',
+                //         smooth: true, //数据光滑过度
+                //         symbol: 'none', //下一个数据点
+                //         stack: 'a',
+                //         areaStyle: {
+                //             normal: {
+                //                 color: 'red'
+                //             }
+                //         },
+                //         data: this.data
+                //     }],
+                //     grid: {
+                //         bottom: '30%',
+                //         left: '20%'
+                //     }
+                // })
 
                 this.bar.setOption({
                     tooltip: {
