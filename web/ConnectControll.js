e902a96015c8570a03e6dfa42bce140e941cbc73
Misaka1001 <eugen0822@outlook.com>
2019-06-11 21:57:42 +0800

    cors

--- web/ConnectControll.js
+++ web/ConnectControll.js
@@ -2,15 +2,16 @@ let express = require('express');
 let ws = require('express-ws');
 let router = express.Router();
 let bodyparser = require('body-parser');
-let client = new express();
 let cors = require('cors')
+let client = new express();
+client.use(cors({
+    origin: ['http://localhost:8080'],
+    methods: ['GET','POST']
+}))
 client.use(express.static('../client'));
 client.use(bodyparser.urlencoded({ extended: true }));
 client.use(bodyparser.json())
-client.use(cors({
-    origin: ['*'],
-    methods: ['GET','POST']
-}))
+
 ws(client);
 
 let net = require('net')
